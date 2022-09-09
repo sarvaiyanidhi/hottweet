aece1e79988dd4df57bcd8c614b1b16feaa3a6df
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2022-09-08 22:32:16 -0400

    [Tweet] Generated Model and Controller for tweets

---
+++ test/controllers/tweet_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class TweetControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get tweet_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get tweet_create_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get tweet_destroy_url
+    assert_response :success
+  end
+end
